add73b037ebbf3ec893fb374e89fa1b8b73a9b0f
Filipe Ribeiro Simões <filipe.rs@puccampinas.edu.br>
2026-02-25 18:34:54 -0300

    html de telas iniciais

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('bem_vindo.html')
+
+@app.route('/home')
+def home_page():
+    return render_template('home.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/registro')
+def registro():
+    return render_template('registro.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
